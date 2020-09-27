f636c53e63a1614ebd5e26e335a25a1ed380a52b
Rocio Batres <jonasgirl21@gmail.com>
2020-09-27 00:05:26 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,7 +8,10 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person 
-      raise 
+      raise PartnerError
+    else 
+      person.partner = self 
+    end 
   end
   
   class PartnerError < StandardError
